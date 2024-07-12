30c78dc59adffe5da743418d40fb8c7c7c04d8c5
Ranjith.Kumar <theranjith@gmail.com>
2024-07-12 18:31:37 +0530

    added label component

---
+++ reusable-components-vite/src/components/Label/Label.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  htmlFor: string;
+  label: string;
+  required?: boolean;
+}
+
+const Label = ({ htmlFor, label, required = false }: Props) => {
+  return (
+    <>
+      <label style={{ display: "block" }} htmlFor={htmlFor}>
+        {label} {required && <span style={{ color: "red" }}> *</span>}
+      </label>
+    </>
+  );
+};
+
+export default Label;
