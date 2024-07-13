74cbc0ccda9b3c4d99885674def71f011f2e405e
Ranjith.Kumar <theranjith@gmail.com>
2024-07-13 18:09:08 +0530

    added registration form story

---
+++ reusable-components-vite/src/stories/RegistrationForm.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import RegistrationForm from "../components/RegistrationForm/RegistrationForm";
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Example/RegistrationForm",
+  component: RegistrationForm,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof RegistrationForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {
+    confirmationMessage: "Thanks for registering",
+    minPasswordLength: 8,
+    onSubmit(user) {
+      console.log(user);
+    },
+  },
+};
