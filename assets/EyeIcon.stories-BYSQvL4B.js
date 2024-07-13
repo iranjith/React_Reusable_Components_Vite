e88a5961f5161283dd4243d4b547708d1f960347
Ranjith.Kumar <theranjith@gmail.com>
2024-07-13 13:58:32 +0530

    Updates

---
+++ assets/EyeIcon.stories-BYSQvL4B.js
@@ -0,0 +1,3 @@
+import{E as t}from"./EyeIcon-CyCIxuDN.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const p={title:"Example/EyeIcon",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},e={args:{}};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const n=["EyeIconExample"];export{e as EyeIconExample,n as __namedExportsOrder,p as default};
