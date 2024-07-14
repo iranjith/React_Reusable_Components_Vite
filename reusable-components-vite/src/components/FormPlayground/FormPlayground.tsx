fed62ff6eb085aead165f76e0d2bb50bb423869f
Ranjith.Kumar <theranjith@gmail.com>
2024-07-14 11:16:45 +0530

    Added folders for form builder

---
+++ reusable-components-vite/src/components/FormPlayground/FormPlayground.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormPlayground = () => {
+  return (
+    <div>FormPlayground</div>
+  )
+}
+
+export default FormPlayground
---
+++ reusable-components-vite/src/components/JSONEditor/JSONEditor.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JSONEditor = () => {
+  return (
+    <div>JSONEditor</div>
+  )
+}
+
+export default JSONEditor
