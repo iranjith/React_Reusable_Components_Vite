2ab7a6669b3f4ce141fcaa8cd085f9008a1da0c9
Ranjith.Kumar <theranjith@gmail.com>
2024-07-31 11:42:42 +0530

    Updates

--- assets/PasswordInput.stories-CbemZDdm.js
+++
@@ -1,9 +0,0 @@
-import{P as a}from"./PasswordInput-Dm3DoTYj.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./EyeIcon-CyCIxuDN.js";import"./TextInput-3p1jaym9.js";import"./Label-CT0VVVKE.js";const l={title:"Components/PasswordInput",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},s={args:{htmlId:"password-input",name:"password",placeholder:"Enter password",showVisibilityToggle:!1,onChange(){}}};var o,e,r;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    htmlId: "password-input",
-    name: "password",
-    placeholder: "Enter password",
-    showVisibilityToggle: false,
-    onChange() {}
-  }
-}`,...(r=(e=s.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const c=["Default"];export{s as Default,c as __namedExportsOrder,l as default};
